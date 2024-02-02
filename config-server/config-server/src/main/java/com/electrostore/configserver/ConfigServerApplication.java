0ced1d364ea3ea32b829963f2c1c0b0b25490570
Gabriel Almeida <almeidaggabriel93@gmail.com>
2024-02-01 21:10:52 -0300

    Organized project structure by adding folders for each microservice to improve code maintainability.

---
+++ config-server/config-server/src/main/java/com/electrostore/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.electrostore.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
---
+++ eureka-sv/eureka-sv/src/main/java/com/electrostore/eurekasv/EurekaSvApplication.java
@@ -0,0 +1,13 @@
+package com.electrostore.eurekasv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSvApplication.class, args);
+	}
+
+}
